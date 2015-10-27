e4bd1a4434f04828a478bcc667b95d998a83cf2b
Max Sauer <msauer@chemaxon.com>
2015-10-27 12:09:52 +0100

    Fix feed to new spec

--- shop/feed.php
+++ shop/feed.php
@@ -24,7 +24,7 @@ $db->query($q);
 $db->next_record();
 
 //write
-$head = "<?xml version=\"1.0\" encoding=\"utf-8\"?><SHOP>";
+$head = "<?xml version=\"1.0\" encoding=\"utf-8\"?><SHOP xmlns=\"http://www.zbozi.cz/ns/offer/1.0\">";
 $footer = "</SHOP>";
 
 fputs($fh, "$head\n");
@@ -33,18 +33,18 @@ while ($db->next_record()) {
 
     $s = $db->get("product_name");
     $towrite .= $s . '<br>';
-    fputs($fh, "<PRODUCT>$s</PRODUCT>\n");
-    fputs($fh, "<DESCRIPTION>". html_entity_decode($db->get("product_s_desc"), ENT_NOQUOTES , "UTF-8")."</DESCRIPTION>\n");
+    fputs($fh, "<PRODUCTNAME>$s</PRODUCTNAME>\n");
+    fputs($fh, "<DESCRIPTION>". strip_tags(html_entity_decode($db->get("product_s_desc"), ENT_NOQUOTES , "UTF-8"))."</DESCRIPTION>\n");
     $url = "<URL>http://www.cyklosauer.cz/shop/?page=shop/flypage&amp;product_id=". $db->get("product_id") ."&amp;</URL>\n";
     fputs($fh, $url);
     fputs($fh, "<ITEM_TYPE>new</ITEM_TYPE>\n");
-    fputs($fh, "<DELIVERY_DATE> ihned </DELIVERY_DATE>\n");
+    fputs($fh, "<DELIVERY_DATE>1</DELIVERY_DATE>\n");
     fputs($fh, "<IMGURL>http://www.cyklosauer.cz/shop/images/shop/product/". $db->get("product_thumb_image") . "</IMGURL>\n");
     $price = $ps_product->get_price($db->get("product_id"));
     $form_price = number_format($price["product_price"],0,',','');
     fputs($fh, "<PRICE_VAT>". $form_price ."</PRICE_VAT>\n");
 
-    fputs($fh, "<CATEGORYTEXT>Sport - Jízdní kola</CATEGORYTEXT>\n");
+    fputs($fh, "<CATEGORYTEXT>Sport | Jízdní kola</CATEGORYTEXT>\n");
     fputs($fh, "\n");
 
     fputs($fh, "</SHOPITEM>\n");
